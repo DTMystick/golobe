36f78d6074e343ce5ccc9f18dfc608db706b716e
djme <d.mouffok@gmail.com>
2024-06-24 15:56:50 +0200

    ajout de destination.html

---
+++ src/Controller/DestinationController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class DestinationController extends AbstractController
+{
+    #[Route('/destination', name: 'app_destination')]
+    public function index(): Response
+    {
+        return $this->render('destination/destination.html.twig', [
+            'controller_name' => 'DestinationController',
+        ]);
+    }
+}
