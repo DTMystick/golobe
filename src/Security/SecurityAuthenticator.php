583607efc4119b5386c487900cec1346c4fd17ed
Dupmehdi <dupuis.mehdi1@gmail.com>
2024-06-26 09:44:38 +0200

    save

--- src/Security/SecurityAuthenticator.php
+++ src/Security/SecurityAuthenticator.php
@@ -49,8 +49,8 @@ class SecurityAuthenticator extends AbstractLoginFormAuthenticator
         }
 
         // For example:
-        return new RedirectResponse($this->urlGenerator->generate('app_create_account'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        return new RedirectResponse($this->urlGenerator->generate('app_home_page'));
+        // throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
     protected function getLoginUrl(Request $request): string
