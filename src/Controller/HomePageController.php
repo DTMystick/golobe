fcda642f3bc759c360d83b268381429180916d22
Damien Tisné <damien.tisne@free.fr>
2024-08-21 12:42:40 +0200

    je dois push

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class HomePageController extends AbstractController
 {
-    #[Route('/homepage', name: 'app_home_page')]
+    #[Route('/', name: 'app_home_page')]
     public function index(): Response
     {
         return $this->render('home_page/homepage.html.twig', [
